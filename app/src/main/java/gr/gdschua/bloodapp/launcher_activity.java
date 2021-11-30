cfdf874f092c8cd7ebd00729c61a77231628ca0a
Thanasis Bibas <than.bimp@gmail.com>
2021-11-30 23:30:54 +0200

    Inital Commit

---
+++ app/src/main/java/gr/gdschua/bloodapp/launcher_activity.java
@@ -0,0 +1,34 @@
+package gr.gdschua.bloodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class launcher_activity extends AppCompatActivity {
+
+    /*This activity should not be displayed if user is logged in!*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        findViewById(R.id.LoginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(launcher_activity.this, login_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.SignupButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(launcher_activity.this,signup_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/gr/gdschua/bloodapp/signup_activity.java
@@ -0,0 +1,14 @@
+package gr.gdschua.bloodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signup_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
