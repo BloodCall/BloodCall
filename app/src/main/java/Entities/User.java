209d2c767f3a085d87f3bc2cace2963f6eff351f
GeorgeK2001 <otenet17@gmail.com>
2021-12-02 18:45:28 +0200

    added login

---
+++ app/src/main/java/Entities/User.java
@@ -0,0 +1,39 @@
+package Entities;
+
+public class User {
+    private String fullName,email,bloodType,uId;
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public User(String fullName, String email, String bloodType) {
+        this.fullName = fullName;
+        this.email = email;
+        this.bloodType = bloodType;
+    }
+
+    public User() {
+    }
+}
