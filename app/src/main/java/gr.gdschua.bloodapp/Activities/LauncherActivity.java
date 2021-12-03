d7a608ab2bffa6162401544184daacc7adaa0828
Athanasios Bimpas <than.bimp@gmail.com>
2021-12-03 10:53:40 +0200

    changed package structure in order to restore firebase database functionality

---
+++ app/src/main/java/gr.gdschua.bloodapp/Activities/LauncherActivity.java
@@ -0,0 +1,52 @@
+package gr.gdschua.bloodapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import gr.gdschua.bloodapp.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    /*This activity should not be displayed if user is logged in!*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LauncherActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.signup_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LauncherActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //TO-BE-IMPLEMENTED
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //Intent intent = new Intent(LauncherActivity.this, MAIN PAGE );
+        }
+    }
+
+}
