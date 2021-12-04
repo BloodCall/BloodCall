52edf1c8b61305b1cc363216833068fe9b608782
GeorgeK2001 <otenet17@gmail.com>
2021-12-04 02:35:03 +0200

    Fixed UID storage

--- app/src/main/java/gr.gdschua.bloodapp/Activities/LoginActivity.java
+++ app/src/main/java/gr.gdschua.bloodapp/Activities/LoginActivity.java
@@ -44,7 +44,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Toast.makeText(LoginActivity.this, "GOOD" + FirebaseAuth.getInstance().getCurrentUser(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "GOOD" + mAuth.getCurrentUser(), Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(LoginActivity.this,"BAD",Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/gr.gdschua.bloodapp/DatabaseAcess/DAOUsers.java
+++ app/src/main/java/gr.gdschua.bloodapp/DatabaseAcess/DAOUsers.java
@@ -1,27 +1,34 @@
 package gr.gdschua.bloodapp.DatabaseAcess;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import gr.gdschua.bloodapp.Activities.SignupActivity;
 import gr.gdschua.bloodapp.Entities.User;
 
 
 public class DAOUsers {
 
-    private DatabaseReference databaseReference;
 
     public DAOUsers() {
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        this.databaseReference = db.getReference().child("Users");
     }
 
     public Task<Void> insertUser(User newUser){
 
         if(newUser==null) {
-            //throw exception
+            Log.e("FirebaseUser","Did not receive user info");
         }
 
-       return databaseReference.push().setValue(newUser);
+       return FirebaseDatabase.getInstance().getReference("Users")
+               .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+               .setValue(newUser);
     }
 }
