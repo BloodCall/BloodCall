cb79211201dbba9a4230b7b2f32ca2aac1a52029
GeorgeK2001 <otenet17@gmail.com>
2021-12-09 00:25:55 +0200

    added show/hide password button

--- app/src/main/java/gr/gdschua/bloodapp/Activities/LoginActivity.java
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/LoginActivity.java
@@ -2,12 +2,16 @@ package gr.gdschua.bloodapp.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-
+import android.widget.ImageView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -18,6 +22,7 @@ import gr.gdschua.bloodapp.R;
 public class LoginActivity extends AppCompatActivity {
     EditText passET;
     EditText emailET;
+    ImageView showPasswordIV;
     private FirebaseAuth mAuth;
 
     @Override
@@ -33,6 +38,30 @@ public class LoginActivity extends AppCompatActivity {
 
         emailET= findViewById(R.id.email_input);
         passET = findViewById(R.id.password_input);
+        showPasswordIV = findViewById(R.id.showPassword);
+        showPasswordIV.setTag(R.drawable.show_password_image);
+
+        showPasswordIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if((Integer)showPasswordIV.getTag() == R.drawable.show_password_image ){
+
+                    passET.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    passET.setSelection(passET.getText().length());
+                    showPasswordIV.setImageDrawable(ContextCompat.getDrawable(LoginActivity.this, R.drawable.hide_password_image));
+                    showPasswordIV.setTag(R.drawable.hide_password_image);
+
+
+                }else if((Integer)showPasswordIV.getTag() == R.drawable.hide_password_image){
+
+                    passET.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    passET.setSelection(passET.getText().length());
+                    showPasswordIV.setImageDrawable(ContextCompat.getDrawable(LoginActivity.this, R.drawable.show_password_image));
+                    showPasswordIV.setTag(R.drawable.show_password_image);
+                }
+            }
+        });
+
         findViewById(R.id.login_auth_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
