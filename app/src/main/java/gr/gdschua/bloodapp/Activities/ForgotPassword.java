cf54c1f82e80f9b224c91437383823cc7de063b5
Bleemoose <35433576+Bleemoose@users.noreply.github.com>
2021-12-09 23:05:27 +0200

    added forgot password functionality

--- app/src/main/java/gr/gdschua/bloodapp/Activities/ForgotPassword.java
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/ForgotPassword.java
@@ -1,11 +1,16 @@
 package gr.gdschua.bloodapp.Activities;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 import gr.gdschua.bloodapp.R;
@@ -34,7 +39,30 @@ public class ForgotPassword extends AppCompatActivity {
         findViewById(R.id.resetPassButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().sendPasswordResetEmail(emailET.getText().toString().trim());
+                String email = emailET.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    emailET.setError("Email is required");
+                    emailET.requestFocus();
+                    return;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    emailET.setError("Please provide a valid email address");
+                    emailET.requestFocus();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Reset email sent successfully ",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"ERROR!!!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
