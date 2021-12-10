73a9351f2e0212fdf847fd40464554a0f18a7975
GeorgeK2001 <otenet17@gmail.com>
2021-12-10 22:28:11 +0200

    added conversion of address to lat,lon

--- app/src/main/java/gr/gdschua/bloodapp/Activities/MapsFragment.java
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/MapsFragment.java
@@ -21,20 +21,10 @@ import gr.gdschua.bloodapp.R;
 public class MapsFragment extends Fragment {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            LatLng sydney = new LatLng(37.962001099999995, 23.7010336);
+            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in PAPPUSPITAL"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
     };
