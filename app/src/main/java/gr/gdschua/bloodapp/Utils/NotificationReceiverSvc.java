e33a9a4072676d74a25631d7ec16d2ffd7755a66
Athanasios Bimpas <than.bimp@gmail.com>
2022-01-13 22:26:29 +0200

    added basic notification functionality

---
+++ app/src/main/java/gr/gdschua/bloodapp/Utils/NotificationReceiverSvc.java
@@ -0,0 +1,109 @@
+package gr.gdschua.bloodapp.Utils;
+
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import gr.gdschua.bloodapp.Activities.MainActivity;
+import gr.gdschua.bloodapp.R;
+
+public class NotificationReceiverSvc extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.d("FCM", "Refreshed token: " + s);
+    }
+
+
+    @Override
+    public void
+    onMessageReceived(RemoteMessage remoteMessage) {
+        // First case when notifications are received via
+        // data event
+        // Here, 'title' and 'message' are the assumed names
+        // of JSON
+        // attributes. Since here we do not have any data
+        // payload, This section is commented out. It is
+        // here only for reference purposes.
+        /*if(remoteMessage.getData().size()>0){
+            showNotification(remoteMessage.getData().get("title"),
+                          remoteMessage.getData().get("message"));
+        }*/
+
+        // Second case when notification payload is
+        // received.
+        if (remoteMessage.getNotification() != null) {
+            showNotification(
+                    remoteMessage.getNotification().getTitle(),
+                    remoteMessage.getNotification().getBody());
+        }
+    }
+
+
+    private RemoteViews getCustomDesign(String title,
+                                        String message) {
+        RemoteViews remoteViews = new RemoteViews(
+                getApplicationContext().getPackageName(),
+                R.layout.notification);
+        remoteViews.setTextViewText(R.id.title, title);
+        remoteViews.setTextViewText(R.id.message, message);
+        remoteViews.setImageViewResource(R.id.icon,
+                R.drawable.custom_fullname_icon);
+        return remoteViews;
+    }
+
+    public void showNotification(String title,
+                                 String message) {
+        Intent intent
+                = new Intent(this, MainActivity.class);
+        String channel_id = "notification_channel";
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent
+                = PendingIntent.getActivity(
+                this, 0, intent,
+                PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder
+                = new NotificationCompat
+                .Builder(getApplicationContext(),
+                channel_id)
+                .setSmallIcon(R.drawable.custom_fullname_icon)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000,
+                        1000, 1000})
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+
+        builder = builder.setContent(
+                getCustomDesign(title, message));
+        NotificationManager notificationManager
+                = (NotificationManager) getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT
+                >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel
+                    = new NotificationChannel(
+                    channel_id, "web_app",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(
+                    notificationChannel);
+        }
+
+        notificationManager.notify(0, builder.build());
+    }
+}
