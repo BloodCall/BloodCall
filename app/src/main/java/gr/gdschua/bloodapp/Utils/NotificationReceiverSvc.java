063f80014d222ddfe06b355d6de0560151e819ed
Athanasios Bimpas <than.bimp@gmail.com>
2022-01-13 23:02:59 +0200

    nothing much

--- app/src/main/java/gr/gdschua/bloodapp/Utils/NotificationReceiverSvc.java
+++ app/src/main/java/gr/gdschua/bloodapp/Utils/NotificationReceiverSvc.java
@@ -48,60 +48,38 @@ public class NotificationReceiverSvc extends FirebaseMessagingService {
         // Second case when notification payload is
         // received.
         if (remoteMessage.getNotification() != null) {
-            showNotification(
-                    remoteMessage.getNotification().getTitle(),
-                    remoteMessage.getNotification().getBody());
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
         }
     }
 
 
-    private RemoteViews getCustomDesign(String title,
-                                        String message) {
+    private RemoteViews getCustomDesign(String title, String message) {
         RemoteViews remoteViews = new RemoteViews(
-                getApplicationContext().getPackageName(),
-                R.layout.notification);
+        getApplicationContext().getPackageName(), R.layout.notification);
         remoteViews.setTextViewText(R.id.title, title);
         remoteViews.setTextViewText(R.id.message, message);
-        remoteViews.setImageViewResource(R.id.icon,
-                R.drawable.custom_fullname_icon);
+        remoteViews.setImageViewResource(R.id.icon, R.drawable.ic_person);
         return remoteViews;
     }
 
-    public void showNotification(String title,
-                                 String message) {
-        Intent intent
-                = new Intent(this, MainActivity.class);
+    public void showNotification(String title, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
         String channel_id = "notification_channel";
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent
-                = PendingIntent.getActivity(
-                this, 0, intent,
-                PendingIntent.FLAG_IMMUTABLE);
-
-        NotificationCompat.Builder builder
-                = new NotificationCompat
-                .Builder(getApplicationContext(),
-                channel_id)
-                .setSmallIcon(R.drawable.custom_fullname_icon)
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat
+                .Builder(getApplicationContext(), channel_id)
+                .setSmallIcon(R.drawable.ic_person)
                 .setAutoCancel(true)
-                .setVibrate(new long[]{1000, 1000, 1000,
-                        1000, 1000})
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
                 .setOnlyAlertOnce(true)
                 .setContentIntent(pendingIntent);
 
-        builder = builder.setContent(
-                getCustomDesign(title, message));
-        NotificationManager notificationManager
-                = (NotificationManager) getSystemService(
-                Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT
-                >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel
-                    = new NotificationChannel(
-                    channel_id, "web_app",
-                    NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(
-                    notificationChannel);
+        builder = builder.setContent(getCustomDesign(title, message));
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id, "bloodcall", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
         }
 
         notificationManager.notify(0, builder.build());
