65159897fcf1f984b85ce2eea157bda657cd6a4e
Athanasios Bimpas <than.bimp@gmail.com>
2022-01-23 15:56:24 +0200

    updated cloud funtions code

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -1,23 +1,27 @@
 const functions = require("firebase-functions");
 
 const admin = require("firebase-admin");
-
+let hospital;
+let topic;
 admin.initializeApp();
 exports.alertAdded = functions.database.ref("/Alerts/{alert_id}")
     .onCreate((snapshot, context) => {
-      const alertData = snapshot.val();
-      admin.messaging().sendToTopic(
-          alertData.bloodType,
-          {
-            notification: {
-              title: "Urgent Blood Call!",
-              body: "Your blood is needed right now!",
-            },
-            data: {
-              bloodType: alertData.bloodType,
-              dateCreated: alertData.dateCreated,
-              owner: alertData.owner,
-            },
-          }
-      );
+      const alertData=snapshot.val();
+      admin.database().ref("/Hospitals/"+alertData.owner)
+          .once("value", (snap) => {
+            console.log(snap.val());
+            hospital=snap.val().name;
+            topic=alertData.bloodType.replace("+", "pos").replace("-", "neg");
+            admin.messaging().sendToTopic(
+                "/topics/"+topic,
+                {
+                  notification: {
+                    title: "Urgent need for "+alertData.bloodType+" blood",
+                    body: "Your blood is needed right now at "+hospital+"!",
+                  },
+                }
+            );
+          }, (errorObject) => {
+            console.log("The read failed: " + errorObject.name);
+          });
     });
