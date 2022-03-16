b9a87efd3c4c4d28a7a2fa3e11423c48d56cfe0d
GeorgeK2001 <otenet17@gmail.com>
2022-03-16 22:37:45 +0200

    added xp incrementation

--- app/src/main/java/gr/gdschua/bloodapp/Activities/HospitalActivities/ScanResultFragment.java
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/HospitalActivities/ScanResultFragment.java
@@ -3,6 +3,7 @@ package gr.gdschua.bloodapp.Activities.HospitalActivities;
 import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,37 +16,27 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+
 import java.util.Objects;
 
+import gr.gdschua.bloodapp.DatabaseAccess.DAOUsers;
+import gr.gdschua.bloodapp.Entities.User;
 import gr.gdschua.bloodapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScanResultFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ScanResultFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
-
+    private DAOUsers daoUsers = new DAOUsers();
+    private User currUser;
 
     public ScanResultFragment() {
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     *
-     * @return A new instance of fragment ScanResultFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ScanResultFragment newInstance(String param1) {
         ScanResultFragment fragment = new ScanResultFragment();
         Bundle args = new Bundle();
@@ -77,6 +68,15 @@ public class ScanResultFragment extends Fragment {
             ResultIV.setColorFilter(ContextCompat.getColor(getContext(), R.color.light_green), android.graphics.PorterDuff.Mode.SRC_IN);
             ResultIV.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_check_circle_outline_24, getContext().getTheme()));
             ResultTV.setText(getString(R.string.qr_succ));
+            daoUsers.getUser().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if(task.isSuccessful()){
+                        currUser = task.getResult().getValue(User.class);
+                        currUser.setXp(currUser.getXp()+300);
+                    }
+                }
+            });
         }
         else if(mParam1.equals("false1")){
             ResultIV.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_remove_circle_outline_24, getContext().getTheme()));
--- app/src/main/java/gr/gdschua/bloodapp/Activities/MarkerInfoFragment.java
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/MarkerInfoFragment.java
@@ -23,14 +23,23 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 
 import java.io.ByteArrayOutputStream;
 
+import gr.gdschua.bloodapp.DatabaseAccess.DAOUsers;
+import gr.gdschua.bloodapp.Entities.User;
 import gr.gdschua.bloodapp.R;
 
 public class MarkerInfoFragment extends DialogFragment {
 
+    private final DAOUsers daoUsers = new DAOUsers();
+    private User currUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -53,18 +62,24 @@ public class MarkerInfoFragment extends DialogFragment {
         }
 
 
+        Button closeButton = view.findViewById(R.id.closeDialogButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
         Button shareButton = view.findViewById(R.id.shareEventButton);
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //convert layout to bitmap to share
                 organizerName.setText(getArguments().get("name").toString());
                 organizerAddress.setText(getArguments().get("address").toString());
-
                 FrameLayout fLayout = inflatedView.findViewById(R.id.frameLayout);
                 Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_blood_drop);
-
-
-
                 Bitmap bm = Bitmap.createBitmap( 1080,2177, Bitmap.Config.ARGB_8888);
                 Canvas c = new Canvas(bm);
                 fLayout.measure(Resources.getSystem().getDisplayMetrics().widthPixels,Resources.getSystem().getDisplayMetrics().heightPixels);
@@ -72,24 +87,40 @@ public class MarkerInfoFragment extends DialogFragment {
                 fLayout.setLayerType(LAYER_TYPE_SOFTWARE, null);
                 fLayout.draw(c);
 
-                if (bm != null) {
+                daoUsers.getUser().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if(task.isSuccessful()){
+                            currUser = task.getResult().getValue(User.class);
+                            if (bm != null) {
 
-                    Toast toast = Toast.makeText(getContext(), "image saved", Toast.LENGTH_LONG);
-                    toast.show();
-                    Intent share = new Intent(Intent.ACTION_SEND);
-                    share.setType("image/jpeg");
-                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                    bm.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-                    String path = MediaStore.Images.Media.insertImage(getContext().getContentResolver(), bm, "Title", null);
-                    Uri imageUri =  Uri.parse(path);
-                    share.putExtra(Intent.EXTRA_STREAM, imageUri);
-                    startActivity(Intent.createChooser(share, "Select"));
+                                Toast toast = Toast.makeText(getContext(), "Image was saved", Toast.LENGTH_LONG);
+                                toast.show();
+                                Intent share = new Intent(Intent.ACTION_SEND);
+                                share.setType("image/jpeg");
+                                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                                bm.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                                String path = MediaStore.Images.Media.insertImage(getContext().getContentResolver(), bm, "Title", null);
+                                Uri imageUri =  Uri.parse(path);
+                                share.putExtra(Intent.EXTRA_STREAM, imageUri);
+                                startActivity(Intent.createChooser(share, "Select"));
+                                currUser.setXp(currUser.getXp()+25);
+                                daoUsers.updateUser(currUser);
+                                dismiss();
 
-                } else {
-                    Toast toast = Toast.makeText(getContext(),
-                            "No image saved.", Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                            } else {
+                                Toast toast = Toast.makeText(getContext(),
+                                        "No image saved.", Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        }
+                    }
+                });
+
+
+
+
+
 
             }
         });
