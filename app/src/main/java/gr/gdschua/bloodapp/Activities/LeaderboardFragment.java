9481fc796004bf62f6c110919b7d00bf5bc84dad
Athanasios Bimpas <than.bimp@gmail.com>
2022-05-16 22:21:50 +0300

    changed leaderboard to community and added tabs to it so we can add a forum fragment later on

--- app/src/main/java/gr/gdschua/bloodapp/Activities/LeaderboardFragment.java
+++ app/src/main/java/gr/gdschua/bloodapp/Activities/LeaderboardFragment.java
@@ -63,16 +63,13 @@ public class LeaderboardFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment LeaderboardFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LeaderboardFragment newInstance(String param1, String param2) {
+    public static LeaderboardFragment newInstance() {
         LeaderboardFragment fragment = new LeaderboardFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -107,7 +104,7 @@ public class LeaderboardFragment extends Fragment {
             super.run();
             URL url = null;
             try {
-                url = new URL("insert a url for a leaderboard function here");
+                url = new URL("https://europe-west1-bloodcall-951a8-default-rtdb.cloudfunctions.net/getUsersforLeaderboard");
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
